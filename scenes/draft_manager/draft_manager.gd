5b8154aa2b7766863688b80f52a1b1961bc65d58
the goblin game <khaaan24@gmail.com>
2024-07-31 21:50:54 -0400

    start of draft manager

--- scenes/draft_manager/draft_manager.gd
+++ scenes/draft_manager/draft_manager.gd
@@ -1,22 +1,19 @@
 extends Node
 
-var current_round = 0
+var cards = load("../cards.json")
+var current_round = 1
+var round_text = "Round %s/3"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hi")
-# set round to 1, max rounds to 3
-	current_round += 1
-# call get_draft_picks
-# call new_draft_round
-
-func _process(delta):
-	$RoundCounter.set_text("hello")
-	
+	new_draft_round()
+	$RoundCounter.set_text(round_text % current_round)	
+	print(cards)
 func new_draft_round():
 # emit get_cards, receive array of randomly chosen cards
 # instantiate 4 cards, each one is given card info
 # reset timer
-	pass
+# increment current_round
+	current_round += 1
 	#$DraftPickTimer.start()
 
 #func get_draft_picks():
